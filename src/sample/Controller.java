bee47236a6e92e7bdf2339a01f89f66e36f5dbdc
Aleksandar Pantic <47724918+Alekpant@users.noreply.github.com>
2020-11-29 21:57:50 +0100

    testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    public Button button2;
+    public Label label2;
+    @FXML
+    private Label label1;
+    @FXML
+    private Button button1;
+
+    public void buttonClck(ActionEvent actionEvent) {
+        label1.setText("Ovo je test");
+    }
+
+    public void buttonClick2(ActionEvent actionEvent) {
+        label2.setText("Ovo je test2");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
 }
