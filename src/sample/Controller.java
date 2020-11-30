b0cbd2d15d1ba7d859c7bc78e6efbb458f76625a
Aleksandar Pantic <47724918+Alekpant@users.noreply.github.com>
2020-11-30 20:49:31 +0100

    Setting scene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,33 +3,31 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-
-    public Button button2;
-    public Label label2;
     @FXML
-    private Label label1;
+    private Canvas canvas;
     @FXML
-    private Button button1;
+    private VBox sideRight;
+    @FXML
+    private VBox sideLeft;
 
-    public void buttonClck(ActionEvent actionEvent) {
-        label1.setText("Ovo je test");
-    }
-
-    public void buttonClick2(ActionEvent actionEvent) {
-        label2.setText("Ovo je test2");
-    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
 
+    public void mouseClicked(MouseEvent mouseEvent) {
+        System.out.println("Clicked on Canvas");
+    }
 }
