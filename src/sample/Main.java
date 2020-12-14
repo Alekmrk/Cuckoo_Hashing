34e798a8ba1f4d67335594db0d6d3dcef952c780
Aleksandar Pantic <47724918+Alekpant@users.noreply.github.com>
2020-12-14 18:08:13 +0100

    Solved bug about lines going too down

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,11 +12,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Cuckoo Hashing");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 1900, 900));
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
